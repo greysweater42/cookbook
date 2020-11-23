53a77a1f60619c0dc3b4f421f48594181aba0275
tomis9 <tomis9@buziaczek.pl>
2020-11-23 12:43:03 +0100

    xgb custom update

--- scratchpad/xgb_custom/main.py
+++ scratchpad/xgb_custom/main.py
@@ -1,52 +1,41 @@
+import random
+import pandas as pd
 import numpy as np
-import xgboost as xgb
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
+url = 'https://raw.githubusercontent.com/agconti/kaggle-titanic/master/data/train.csv'
+df = pd.read_csv(url)
 
-def gradient(predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
-    y = dtrain.get_label()
-    return (np.log1p(predt) - np.log1p(y)) / (predt + 1)
+df = df[["Survived", "Sex", "Pclass", "Age"]].dropna()
+y = df["Survived"]
+X = df[["Sex", "Pclass", "Age"]]
+X["Sex"] = np.where(X["Sex"] == "male", 1, 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+sum(y_test) / len(y_test)
 
-def hessian(predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
-    y = dtrain.get_label()
-    return ((-np.log1p(predt) + np.log1p(y) + 1) /
-            np.power(predt + 1, 2))
+# decision tree
+dtc = DecisionTreeClassifier()
+dtc.fit(X_train, y_train)
+accuracy_score(dtc.predict(X_test), y_test)
 
-def squared_log(predt: np.ndarray,
-                dtrain: xgb.DMatrix) -> Tuple[np.ndarray, np.ndarray]:
-    '''Squared Log Error objective. A simplified version for RMSLE used as
-    objective function.
-    '''
-    predt[predt < -1] = -1 + 1e-6
-    grad = gradient(predt, dtrain)
-    hess = hessian(predt, dtrain)
-    return grad, hess
+# random forest
+forest = []
+k = round(len(X_train) * 0.5)
+n = len(X_train)
+for i in range(200):
+    columns = [True] + random.choices([True, False], k=2)
+    random.shuffle(columns)
+    rows = random.sample(range(n), k=k)
+    X_rf = X_train.iloc[rows, columns]
+    y_rf = y_train.iloc[rows]
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_rf, y_rf)
+    forest.append((dtc, columns))
 
-xgb.train({'tree_method': 'hist', 'seed': 1994},  # any other tree method is fine.
-           dtrain=dtrain,
-           num_boost_round=10,
-           obj=squared_log)
+votes = np.array([dtc.predict(X_test.iloc[:,columns]) for dtc, columns in forest])
+y_hat = np.mean(votes, axis=0)
+accuracy_score(y_hat > 0.5, y_test)
 
-
-from scipy.stats import lognorm, norm, gamma, gaussian_kde
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-
-y_lognorm = lognorm.rvs(s=0.5, size=1000)
-
-m, x, _ = plt.hist(y_lognorm, density=True, bins=50)
-density = gaussian_kde(y_lognorm)
-plt.plot(x, density(x))
-
-y_gamma = gamma.rvs(a=3, size=1000)
-m, x, _ = plt.hist(y_gamma, density=True, bins=50)
-density = gaussian_kde(y_gamma)
-plt.plot(x, density(x))
-
-e = norm.rvs(size=1000) / 10
-
-import xgboost as xgb
-
-from xgboost import XGBRegressor
-
-xgb = XGBRegressor()
-xgb.fit()
+# gradient boosting
